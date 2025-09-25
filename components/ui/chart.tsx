43df7e524f5160a6802dd86dbf89e0590c632348
Benjameabi <beingbenj@gmail.com>
2025-09-25 19:54:20 +0200

    dark mode

--- components/ui/chart.tsx
+++ components/ui/chart.tsx
@@ -5,8 +5,8 @@ import * as RechartsPrimitive from "recharts";
 
 import { cn } from "./utils";
 
-// Format: { THEME_NAME: CSS_SELECTOR }
-const THEMES = { light: "", dark: ".dark" } as const;
+// Light mode only
+const THEMES = { light: "" } as const;
 
 export type ChartConfig = {
   [k in string]: {
