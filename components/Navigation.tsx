706b7b9d1081fe217301b2d50b28c9509ebe57ac
Benjameabi <beingbenj@gmail.com>
2025-10-22 21:13:06 +0200

    spacing

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -31,7 +31,7 @@ export function Navigation({
       initial={{ opacity: 0, y: -30 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.8, delay: 0.2 }}
-      className={"absolute left-0 right-0 top-[40px] md:top-[48px] lg:top-0 z-50 bg-transparent"}
+      className={"absolute left-0 right-0 top-[40px] md:top-[48px] lg:top-16 z-50 bg-transparent"}
     >
       <div className="container mx-auto px-3 sm:px-4 md:px-6 bg-transparent" style={{ height: isShrunk ? '92px' : '112px', transition: 'height 300ms ease' }}>
         <div className="flex items-center h-full relative">
